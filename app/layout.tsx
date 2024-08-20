8517ed9d0a5d62329cf5398e347886b5b358490d
lindsaytoffolo <lindsaytoffolo@gmail.com>
2024-08-19 22:57:08 -0700

    fix duplicate layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/app/ui/global.css";
 import { defaultFont } from "@/app/ui/fonts";
 import { Metadata } from "next";
+import SideNav from "@/app/ui/dashboard/sidenav";
 
 export const metadata: Metadata = {
     title: {
@@ -11,10 +12,19 @@ export const metadata: Metadata = {
     metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
 };
 
+export const experimental_ppr = true;
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={`${defaultFont.className} antialiased`}>{children}</body>
+            <body className={`${defaultFont.className} antialiased`}>
+                <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+                    <div className="w-full flex-none md:w-72">
+                        <SideNav />
+                    </div>
+                    <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+                </div>
+            </body>
         </html>
     );
 }
