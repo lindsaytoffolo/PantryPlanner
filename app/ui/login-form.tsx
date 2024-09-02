693d80eb1a29c5abee0fecb5b91cfc2ff96d9008
lindsaytoffolo <lindsaytoffolo@gmail.com>
2024-09-01 22:04:38 -0700

    add loading state to login button

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -69,9 +69,9 @@ export default function LoginForm() {
                 <Button
                     type="submit"
                     className="mt-4 w-full"
-                    aria-disabled={isPending}
+                    loading={isPending}
                 >
-                    Log in{" "}
+                    Log in
                     <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
                 </Button>
                 <div
